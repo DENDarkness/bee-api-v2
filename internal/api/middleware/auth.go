dfa9d44df0aa0c2b996d2b161e10204399ce6b3f
Denis Bulavin <d.bulavin81@yandex.ru>
2022-01-20 17:01:45 +0300

    fix authorization token

--- internal/api/middleware/auth.go
+++ internal/api/middleware/auth.go
@@ -9,11 +9,11 @@ import (
 func Auth(token string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		t := ctx.GetHeader("Authorization")
-		if token == "" {
+		if t == "" {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Token is empty"})
 			return
 		}
-		if token != t {
+		if t != token {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid Token"})
 			return
 		}
